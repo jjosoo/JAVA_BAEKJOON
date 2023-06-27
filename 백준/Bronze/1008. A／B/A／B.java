d4493145f3f3cbc89d5305139df46f573ed61803
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-06-27 14:15:00 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int inum1 = 0;
+		int inum2 = 0;
+		double div;
+		
+		inum1 = sc.nextInt();
+		inum2 = sc.nextInt();
+		
+		div = (double)inum1/inum2;
+		System.out.println(div);
+	}
+	
+}
