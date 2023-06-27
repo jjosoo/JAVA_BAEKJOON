16224f506abbc3d9c81df62964bc534b866b22e3
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-06-27 11:49:10 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int inum1 = 0;
+		int inum2 = 0;
+		int sum = 0;
+		
+		inum1 = sc.nextInt();
+		inum2 = sc.nextInt();
+		
+		sum = inum1 + inum2;
+		System.out.printf("%d",sum);
+	}
+	
+}
