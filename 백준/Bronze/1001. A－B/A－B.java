079b7ac70a18fe0483739d310cfe70c0297531ba
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-06-27 11:50:11 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int inum1 = 0;
+		int inum2 = 0;
+		int minus = 0;
+		
+		inum1 = sc.nextInt();
+		inum2 = sc.nextInt();
+		
+		minus = inum1 - inum2;
+		System.out.printf("%d",minus);
+	}
+	
+}
