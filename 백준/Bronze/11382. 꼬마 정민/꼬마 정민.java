54b22b5b7e01ade73f7595f328847078a659942d
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-06-27 17:20:15 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		long lnum1 = sc.nextLong();
+		long lnum2 = sc.nextLong();
+		long lnum3 = sc.nextLong();
+		
+		System.out.println(lnum1+lnum2+lnum3);
+	}
+}
