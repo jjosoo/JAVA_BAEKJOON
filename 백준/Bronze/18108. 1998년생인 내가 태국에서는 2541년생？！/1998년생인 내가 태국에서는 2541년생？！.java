56596989a59b2c16d25609c3404f9a7a00be045d
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-06-27 16:27:52 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 224 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int inum = sc.nextInt();
+		System.out.printf("%d", inum-543);
+	}
+	
+}
