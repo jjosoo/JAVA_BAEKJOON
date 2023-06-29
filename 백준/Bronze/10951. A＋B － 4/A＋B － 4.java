a3bed4c6a5a3448aeef8d315ff8cb26338b5dee8
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-06-29 22:52:16 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int inum1 = sc.nextInt();
+			int inum2 = sc.nextInt();
+			
+			System.out.println(inum1+inum2);
+		}
+	}	
+}
