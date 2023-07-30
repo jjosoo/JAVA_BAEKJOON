6e28c3e51b92e2ccec40a7dfb28ff2e0bcff4806
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-07-31 00:36:36 +0900

    Update 공 넣기.java

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int inum = sc.nextInt();
 		int[] ibasket = new int[inum];
@@ -13,17 +14,11 @@ public class Main {
 			int j = sc.nextInt();
 			int k = sc.nextInt();
 			for(int fi2=i-1;fi2<j;fi2++) {
-				if(fi2 >= 0 && fi2 < inum) { // validation check
-					ibasket[fi2]=k;
-				} else {
-					System.out.println("Index out of range");
-				}
+				ibasket[fi2]=k;
 			}
 		}
-		for(int i=0;i<inum;i++) { // Changed from icount to inum
+		for(int i=0;i<inum;i++) {
 			System.out.println(ibasket[i]);
 		}
-
-		sc.close(); // Scanner object closed
 	}
 }
