b718d81b168209c16c587e82a24c31d2845096e0
jjosoo <130520024+jjosoo@users.noreply.github.com>
2023-08-01 23:26:58 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int iresult = str.charAt(0);
+		
+		System.out.println(iresult);
+		
+	}
+}
